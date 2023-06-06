58bd5ae1f7fbd6630e99620e15e4233bac395656
lamnhph18826 <119296072+lamnhph18826@users.noreply.github.com>
2023-06-07 00:15:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
 //var apartmentRouter = require('./routes/apartment');
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 //app.use('/apartment', apartmentRouter);
 
 // catch 404 and forward to error handler
