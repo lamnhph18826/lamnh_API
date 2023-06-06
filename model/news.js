cd51c486a46ba7f3b19e8d80fc3fbecefe8a96b0
lamnhph18826 <119296072+lamnhph18826@users.noreply.github.com>
2023-06-06 20:01:16 +0700

    update

--- model/news.js
+++ model/news.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://admin:lamnhph18826@cluster0.tdhtc06.mongodb.net/news').then(data=>{
-    if (data!=null){
-        console.log("connect news success :'>")
-    }
-});
-
-
-const {Schema} = mongoose;
-const News = new Schema({
-    id : String,
-    title : String,
-    message : String,
-    date:Date,
-    link :Object
-})
-const news = mongoose.model('news',News);
-module.exports = news
+// const mongoose = require('mongoose');
+// mongoose.set('strictQuery', true);
+// mongoose.connect('mongodb+srv://admin:lamnhph18826@cluster0.tdhtc06.mongodb.net/news').then(data=>{
+//     if (data!=null){
+//         console.log("connect news success :'>")
+//     }
+// });
+//
+//
+// const {Schema} = mongoose;
+// const News = new Schema({
+//     id : String,
+//     title : String,
+//     message : String,
+//     date:Date,
+//     link :Object
+// })
+// const news = mongoose.model('news',News);
+// module.exports = news
